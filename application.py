6cb21634c0392e6e68a2637de8a6f1e09b4f1676
SRDJ7 <83389994+SRDJ7@users.noreply.github.com>
2023-12-07 01:16:41 +0530

    Update application.py
    Again changed port to 8000

--- application.py
+++ application.py
@@ -323,4 +323,4 @@ def ask():
 # The port was 5000, but Azure is pinging on 8000.
 # It is giving application error.....
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
